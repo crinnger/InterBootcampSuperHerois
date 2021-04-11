39a15bc2050e3b2e6de9b57a46c36c04820be73c
Crinnger <crinnger@gmail.com>
2021-04-11 18:41:48 -0400

    first commit

---
+++ src/main/java/br/com/crinnger/InterBootcampSuperHerois/InterBootcampSuperHeroisApplication.java
@@ -0,0 +1,13 @@
+package br.com.crinnger.InterBootcampSuperHerois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterBootcampSuperHeroisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterBootcampSuperHeroisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/crinnger/InterBootcampSuperHerois/InterBootcampSuperHeroisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crinnger.InterBootcampSuperHerois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterBootcampSuperHeroisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
